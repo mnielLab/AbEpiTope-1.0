9713d9abf0bc1f5a68c959eb52de891490e5d7c4
joacli <joacli@g-11-g0014.cm.cluster>
2024-09-09 15:13:08 +0200

    added setup file git pip install

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your-package-name',
+    name='antiinternet',
     version='0.1',
-    packages=find_packages(where='src'),  # Look for packages in the 'src' directory
-    package_dir={'': 'src'},  # Set package root to 'src'
+    packages=find_packages(),  
     include_package_data=True,  # Include non-code files specified in MANIFEST.in
     package_data={
-        'your_package': [
-            'models/modeltype1/*.pt',  # Include all .pt files for modeltype1
-            'models/modeltype2/*.pt',  # Include all .pt files for modeltype2
+        'antiinternet': [
+            'models/antiinternet1.0/*.pt',  # Include all .pt files for AntiInterNet-1.0
+            'models/antiscout1.0/*.pt',  # Include all .pt files for AntiScout-1.0
+            'models/hmm_antibody_identification/*.hmm' # include hmm models for antibody identification
         ]
     },
     install_requires=[
         # List your package's dependencies here
     ],
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A short description of your package',
+    author='Joakim Clifford',
+    author_email='cliffordjoakim@gmail.com',
+    description='AntiInterNet-1.0: Access the accuracy of modelled antibody-antigen interfaces (PDB/CIF of AlphaFold or experimentally solved structures). AntiScout-1.0: Select the most likely antibody to bind a given antigen from a pool of candidates (PDB/CIF of AlphaFold or experimentally solved structures)',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/your-repo',
+    url='https://github.com/mnielLab/AntiInterNet-1.0.git',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
